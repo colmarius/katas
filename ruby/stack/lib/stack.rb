80d0a764ee4d9f4f823279748b7d36ddef82b50c
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-08 13:19:32 +0100

    Extract Stack::Bounded class.

--- ruby/stack/lib/stack.rb
+++ ruby/stack/lib/stack.rb
@@ -1,36 +1,12 @@
-class Stack
+require 'stack/bounded'
+
+module Stack
   IllegalCapacity = Class.new(RuntimeError)
   Overflow = Class.new(RuntimeError)
   Underflow = Class.new(RuntimeError)
 
-  attr_reader :size, :capacity
-
-  def initialize(capacity)
-    @size = 0
-    @capacity = capacity
-    @elements = Array.new(capacity)
-  end
-  private_class_method :new
-
-  def empty?
-    @size == 0
-  end
-
-  def push(element)
-    raise Overflow.new if size == capacity
-    @elements[@size] = element
-    @size += 1
-    nil
-  end
-
-  def pop
-    raise Underflow.new if size == 0
-    @size -= 1
-    @elements[@size]
-  end
-
   def self.make(capacity)
     raise IllegalCapacity.new if capacity < 0
-    new(capacity)
+    Bounded.new(capacity)
   end
 end
---
+++ ruby/stack/lib/stack/bounded.rb
@@ -0,0 +1,28 @@
+module Stack
+  class Bounded
+    attr_reader :size, :capacity
+
+    def initialize(capacity)
+      @size = 0
+      @capacity = capacity
+      @elements = Array.new(capacity)
+    end
+
+    def empty?
+      @size == 0
+    end
+
+    def push(element)
+      raise Overflow.new if size == capacity
+      @elements[@size] = element
+      @size += 1
+      nil
+    end
+
+    def pop
+      raise Underflow.new if size == 0
+      @size -= 1
+      @elements[@size]
+    end
+  end
+end
