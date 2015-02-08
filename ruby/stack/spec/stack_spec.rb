a24dc753eea28f0580edaa13d975a26b8da3bc1e
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-08 12:31:07 +0100

    Refactor: extract stack variable.
    Let is a memoized helper method. The value will be cached across multiple calls in the same example but not across examples.

--- ruby/stack/spec/stack_spec.rb
+++ ruby/stack/spec/stack_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 require 'stack'
 
 RSpec.describe Stack do
+  let(:stack) { Stack.new }
 
   it 'newly created stacks should be empty' do
-    stack = Stack.new
     expect(stack.empty?).to be true
     expect(stack.size).to eq 0
   end
 
   it 'after one push stack size should be one' do
-    stack = Stack.new
     stack.push(1)
     expect(stack.size).to eq 1
     expect(stack.empty?).to be false
