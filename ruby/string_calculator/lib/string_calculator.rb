75fff6f0b7d3bddc440d01369ec381dd3320a2ce
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-21 20:49:35 +0100

    Has add method takes numbers. Returns zero for empty string.

--- ruby/string_calculator/lib/string_calculator.rb
+++ ruby/string_calculator/lib/string_calculator.rb
@@ -1,2 +1,5 @@
 module StringCalculator
+  def self.add(numbers)
+    0
+  end
 end
--- ruby/string_calculator/spec/string_calculator_spec.rb
+++ ruby/string_calculator/spec/string_calculator_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe StringCalculator do
   it 'should be defined' do
     expect(StringCalculator).to_not be_nil
   end
+
+  describe '#add' do
+    it 'should return zero for empty string' do
+      expect(StringCalculator.add('')).to eq 0
+    end
+  end
 end
