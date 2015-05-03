80377d56c2e4b5f1114becdc80fcd81b30a1a0ab
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-03 11:58:34 +0200

    Extract private honorific? method.

--- ruby/name_inverter/lib/name_inverter.rb
+++ ruby/name_inverter/lib/name_inverter.rb
@@ -3,12 +3,18 @@ class NameInverter
     return '' if name.nil? || name.empty?
 
     names = name.split(' ')
-    names.delete_at(0) if names[0] =~ /Mr\.|Mrs\./
+    names.delete_at(0) if honorific?(names[0])
 
     if names.length == 1
       return names[0]
     else
       return "#{names[1]}, #{names[0]}"
     end
+  end
+
+  private
+
+  def honorific?(name)
+    name =~ /Mr\.|Mrs\./
   end
 end
