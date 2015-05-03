80d4ea7e1395f75feac09ba5b06fb8c1922c73f7
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-03 11:28:42 +0200

    Extract name_inverter.

--- ruby/name_inverter/spec/name_inverter_spec.rb
+++ ruby/name_inverter/spec/name_inverter_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 require 'name_inverter'
 
 RSpec.describe NameInverter do
+  let(:name_inverter) { NameInverter.new }
+
   describe '#invert_name' do
     it 'return empty string for nil name' do
-      name_inverter = NameInverter.new
       expect(name_inverter.invert_name(nil)).to eq('')
     end
 
     it 'return empty string for empty name' do
-      name_inverter = NameInverter.new
       expect(name_inverter.invert_name('')).to eq('')
     end
   end
