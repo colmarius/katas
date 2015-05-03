d1dabd2beb8c3b6747bfb1ff0366276cd7816492
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-03 12:14:35 +0200

    Refactoring is the reward for making tests pass.

--- ruby/name_inverter/lib/name_inverter.rb
+++ ruby/name_inverter/lib/name_inverter.rb
@@ -2,26 +2,45 @@ class NameInverter
   def invert_name(name)
     return '' if name.nil? || name.empty?
 
-    names = name.split(' ')
-    names.delete_at(0) if honorific?(names[0])
-
-    if names.length == 1
-      return names[0]
-    else
-      post_nominal = ''
-
-      if names.length > 2
-        post_nominals = names.slice(2, names.length)
-        post_nominal = post_nominals.join(' ')
-      end
-
-      return "#{names[1]}, #{names[0]} #{post_nominal}".strip
-    end
+    format_name(remove_honorifics(split_names(name)))
   end
 
   private
 
+  def split_names(name)
+    name.split(' ')
+  end
+
+  def remove_honorifics(names)
+    names.delete_at(0) if honorific?(names[0])
+    names
+  end
+
   def honorific?(name)
     name =~ /Mr\.|Mrs\./
+  end
+
+  def format_name(names)
+    return names[0] if names.length == 1
+
+    format_multi_name(names)
+  end
+
+  def format_multi_name(names)
+    first_name = names[0]
+    last_name = names[1]
+
+    return "#{last_name}, #{first_name} #{post_nominal(names)}".strip
+  end
+
+  def post_nominal(names)
+    all_post_nominals = ''
+
+    if names.length > 2
+      post_nominals = names.slice(2, names.length)
+      all_post_nominals = post_nominals.join(' ')
+    end
+
+    all_post_nominals
   end
 end
